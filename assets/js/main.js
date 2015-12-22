5c4d5b0fbad77f61b4258c5fbc20a0133c14c956
vladhd93 <vladhd@ukr.net>
2015-12-23 00:33:01 +0200

    create grid portfolio

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,3 @@
-/*
-	Overflow by HTML5 UP
-	html5up.net | @n33co
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
-*/
 
 (function($) {
 
@@ -12,7 +7,7 @@
 			fullScreenHeader: true,
 
 		// Parallax background effect?
-			parallax: true,
+			parallax: false,
 
 		// Parallax factor (lower = more intense, higher = less intense).
 			parallaxFactor: 10
